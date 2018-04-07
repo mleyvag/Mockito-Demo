adeab1048e49e7aecf518c03686545a0d53e1237
Miguel Leyva <leyvaguadalupemiguel@gmail.com>
2018-04-07 18:25:59 -0500

    Mockito simple

--- src/test/java/service/CursoServiceTest.java
+++ src/test/java/service/CursoServiceTest.java
@@ -1,6 +1,8 @@
 package service;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +14,27 @@ import model.Estudiante;
 public class CursoServiceTest {
 
 	@Test
-	public void obtenerMejorEstudianteTest() {
+	public void obtenerMejorEstudianteTest_sinMock() {
 		CursoService cursoService = new CursoService(new EstudianteServiceImpl());
+		int mejorNota = cursoService.obtenerMejorEstudiante().getNota();
+		assertEquals(15, mejorNota);
+	}
+	
+	@Test
+	public void obtenerMejorEstudianteTest_conMock() {
+		EstudianteService miMock = mock(EstudianteService.class);
+		List<Estudiante> todos = new ArrayList<>();
+		Estudiante estudiante1 = new Estudiante();
+		estudiante1.setNombre("Miguel");
+		estudiante1.setNota(15);
+		todos.add(estudiante1);
+		Estudiante estudiante2 = new Estudiante();
+		estudiante2.setNombre("Angel");
+		estudiante2.setNota(14);
+		todos.add(estudiante2);
+		//Entonces no habría la necesidad de crear implementaciones para poder usar mi Servicio Estudiante
+		when(miMock.todosLosEstudiantes()).thenReturn(todos);
+		CursoService cursoService = new CursoService(miMock);
 		int mejorNota = cursoService.obtenerMejorEstudiante().getNota();
 		assertEquals(15, mejorNota);
 	}
